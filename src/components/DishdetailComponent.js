fe7e6c8c5daa4de942816ee43bc4fc5b4ceb9922
cleobatista <batista.cleo@gmail.com>
2019-09-02 10:31:33 -0300

    Single Page Application Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 
-	function renderComments(comments) {
+	function RenderComments({comments}) {
 		if (comments != null) {
 			const rendered_comments = comments.map((comment) => {
 				return <li key={comment.id} ><p>{comment.comment}</p>
@@ -22,7 +23,7 @@ import Moment from 'react-moment';
 			return <div></div>
 	}
 
-	function renderDish(dish) {
+	function RenderDish({dish}) {
 		return(
 			<Card>
 	      <CardImg top src={dish.image} alt={dish.name} />
@@ -39,15 +40,26 @@ import Moment from 'react-moment';
 		if (dish != null) {
 			return (
 				<div className="container">
-					<div className="row">
-						<div className="col-12 col-md-5 m-1">
-				      {renderDish(dish)}
-						</div>
-						<div className="col-12 col-md-5 m-1">
-							{renderComments(dish.comments)}
-						</div>
-					</div>
-				</div>
+          <div className="row">
+              <Breadcrumb>
+
+                  <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                  <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+              </Breadcrumb>
+              <div className="col-12">
+                  <h3>{props.dish.name}</h3>
+                  <hr />
+              </div>                
+          </div>
+          <div className="row">
+              <div className="col-12 col-md-5 m-1">
+                  <RenderDish dish={props.dish} />
+              </div>
+              <div className="col-12 col-md-5 m-1">
+                  <RenderComments comments={props.comments} />
+              </div>
+          </div>
+        </div>
 	    );
 		}
 		else
